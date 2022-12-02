948bb27e816e321c03bbbcb49fce811203f5c80f
minturtle <minseok0425@kakao.com>
2022-12-02 12:47:52 +0900

    ?

--- socket-server/server/image_service.py
+++ socket-server/server/image_service.py
@@ -1,48 +1,17 @@
-import tkinter as tk
 import cv2
-from PIL import Image
-from PIL import ImageTk
-from tkinter import SUNKEN, filedialog
+def get_person():
+    # 준비
+    face_cascade = cv2.CascadeClassifier(
+        './data/haarcascades/haarcascade_frontalface_default.xml')
+
+    image = cv2.imread('C:\\Users\\user\\Desktop\\test_img.jpg')
+    grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 
-def select_image():
-    global panelA, panelB, entry
-    path = filedialog.askopenfilename()
-    image = cv2.imread(path)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (11, 11), 0)
-    canny = cv2.Canny(blur, 30, 100)
-    dilated = cv2.dilate(canny, (1, 1), iterations=0)
-    cnt, hierarchy = cv2.findContours(dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    rgb2 = cv2.drawContours(rgb, cnt, -1, (0, 255, 0), 2)
+    faces = face_cascade.detectMultiScale(grayImage, 1.03, 5)
 
-    image = Image.fromarray(image)
-    image = ImageTk.PhotoImage(image)
-
-    rgb2 = Image.fromarray(rgb2)
-    rgb2 = ImageTk.PhotoImage(rgb2)
-
-    panelA = tk.Label(image=image)
-    panelA.image = image
-    panelA.grid(row=0, column=0)
-
-    panelB = tk.Label(image=rgb2)
-    panelB.image = rgb2
-    panelB.grid(row=0, column=1)
-
-    entry = tk.Entry(width=30)
-    entry.grid(row=1, column=0, columnspan=2)
-    entry.insert(0, 'Number of Objects: ')
-    entry.delete(20, tk.END)
-    entry.insert(20, len(cnt))
+    print(faces.shape)
+    print("Number of faces detected: " + str(faces.shape[0]))
 
 if __name__ == "__main__":
-    window = tk.Tk()
-    panelA = None
-    panelB = None
-    entry = None
-    button = tk.Button(text="select image", width=40, relief=SUNKEN, command=select_image)
-    button.grid(row=2, column=0, columnspan=2, pady=10)
-
-    window.mainloop()
+    get_person()
